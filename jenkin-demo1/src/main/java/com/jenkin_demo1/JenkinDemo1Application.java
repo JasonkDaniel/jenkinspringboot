3891d0ab41399442b1ee178bb64c6a9eae666b16
jasonkdaniel <jasonkdaniel@yahoo.com>
2024-11-16 22:55:40 +0530

    Initial Spring Boot application with Jenkinsfile”

---
+++ jenkin-demo1/src/main/java/com/jenkin_demo1/JenkinDemo1Application.java
@@ -0,0 +1,13 @@
+package com.jenkin_demo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinDemo1Application.class, args);
+	}
+
+}
---
+++ jenkin-demo1/src/test/java/com/jenkin_demo1/JenkinDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkin_demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
